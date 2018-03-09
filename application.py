68adf424c8b9f754f02f40f369a1c2e1dd649b37
PC <patrickchang@gmail.com>
2018-03-09 12:32:13 -0600

    added host to app.py

--- application.py
+++ application.py
@@ -44,4 +44,4 @@ def profile2(name):
     return render_template("profile.html",name=name)
 
 if __name__ == "__main__":
-    application.run(debug=True)
+    application.run(host='0.0.0.0',debug=True)
