237c820d7824360fe2f20df7847adefeffae2e1c
EC2 Default User <ec2-user@ip-172-31-29-253.us-west-2.compute.internal>
2018-03-24 03:46:34 +0000

    Editing Sphinx

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://pangur:pangurpoo@policedb.citpvzdsvfxn.us-west-2.rds.amazonaws.com:3306/policedb'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-socrataKey = 'EjrjYzG6YAkBx7bPBzME8jD4c'
